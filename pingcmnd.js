10a135d5d54bd281698f26442ae7a008bbc19a7f
Helix <68465860+HelixDev@users.noreply.github.com>
2020-08-03 20:01:26 +0200

    Create pingcmnd.js

---
+++ pingcmnd.js
@@ -0,0 +1,9 @@
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(!message.content.startsWith(config.prefix)) return;
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if(command === "ping") {
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+  }
